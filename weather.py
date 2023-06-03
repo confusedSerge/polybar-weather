0cf5d220eb7651728f20f3b4cb2f01d79a451929
confusedSerge <serge.kotchourko@gmail.com>
2023-06-03 08:03:45 +0200

    updated weather to new font

--- weather.py
+++ weather.py
@@ -8,13 +8,17 @@ import sys
 config = configparser.ConfigParser()
 config.read(f'{sys.argv[1]}/config.ini')
 
-dict_weather_group_icon = { 200: '', 300: '', 500: '', 600: '', 700: '', 800: ['', ''], 801: ['', ''], 802: '', 803: '', 804: ''}
+dict_weather_group_icon = { 200: '', 300: '', 500: '', 600: '', 700: '', 800: ['', ''], 801: ['', ''], 802: '', 803: '', 804: ''}
 
 URL = 'http://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units={}&exclude=minutely,hourly,daily,alerts&appid={}'
 url_formated = URL.format(config['OPENWEATHERMAP']['lat'], config['OPENWEATHERMAP']['lon'], config['OPENWEATHERMAP']['units'], config['OPENWEATHERMAP']['appid'])
 
 # get data
-json_answer = requests.get(url_formated).json()
+try:
+    json_answer = requests.get(url_formated).json()
+except:
+    print('')
+    exit()
 current = json_answer['current']
 
 # get weather icon
